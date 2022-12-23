cf6407251b808384f741af6a10425a08037b76c8
Lespingal <pierre.lespingal@gmail.com>
2022-12-23 12:19:06 +0100

    fix: Fix screen overflow

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,27 +5,30 @@
   import { onMount } from 'svelte';
   import Background from '$lib/design/Background.svelte';
 
+  // Pattern to show transitions on first load
   let display = false;
   onMount(() => {
     display = true;
   });
 </script>
 
-<div class="w-full h-full text-gray-900">
-  <Background />
-  {#if display}
-    <div class="z-10 w-full h-full flex flex-col">
-      <div class="contents md:hidden">
-        <Header />
-      </div>
-      <div class="flex grow">
-        <div class="w-38 h-full hidden md:flex items-center justify-center">
-          <NavigationBar />
+<div class="relative w-screen h-screen overflow-hidden">
+  <div class="contents text-gray-800">
+    <Background />
+    {#if display}
+      <div class="z-10 w-full h-full flex flex-col">
+        <div class="contents md:hidden">
+          <Header />
         </div>
-        <div class="relative flex mb-32 md:mb-0 grow justify-center items-center">
-          <slot />
+        <div class="flex grow">
+          <div class="w-38 h-full hidden md:flex items-center justify-center">
+            <NavigationBar />
+          </div>
+          <div class="relative flex mb-32 md:mb-0 grow justify-center items-center">
+            <slot />
+          </div>
         </div>
       </div>
-    </div>
-  {/if}
+    {/if}
+  </div>
 </div>
